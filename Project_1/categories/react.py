25c67ae99a9bace6d48006c7816eb1cd91e01008
Roderick <enuke4@gmail.com>
2022-04-18 22:19:59 -0600

    refactor

--- Project_1/categories/react.py
+++ Project_1/categories/react.py
@@ -3,13 +3,18 @@ def react_quiz():
     print("Q1: What are the 2 ways to pass data to React components? \n")
 
     answer = input("A: ")
+    correctAns = 0
+    wrongAns = 0
 
     if answer.lower() == "props and context api" or "context api and props":
       print ("Your answer is ✅ ! \n")
+      correctAns = correctAns + 1
+      print(correctAns)
     
     else:
       print ("Your answer is ❌ ! \n")
-
+      wrongAns = wrongAns + 1
+      print(wrongAns)
     
     print("Q2: What is JSX? \n")
 
@@ -17,9 +22,13 @@ def react_quiz():
 
     if answer.lower() != "javascript xml":
       print ("Your answer is ❌ ! \n")
+      wrongAns = wrongAns + 1
+      print(wrongAns)
     
     else:
       print ("Your answer is ✅ ! \n")
+      correctAns = correctAns + 1
+      print(correctAns)
 
     
     print("Q3: Name 2 types of React components? \n")
@@ -28,7 +37,11 @@ def react_quiz():
 
     if answer.lower() == "function and class" or "class and function": 
       print ("Your answer is ✅ ! \n")
+      correctAns = correctAns + 1
+      print(correctAns)
     
     else:
       print ("Your answer is ❌ ! \n")
+      wrongAns = wrongAns + 1
+      print(wrongAns)
 
---
+++ Project_1/counter.py
@@ -0,0 +1,10 @@
+def correctAns():
+    counter = 0
+    counter += 1
+    print(counter)
+
+def wrongAns():
+    counter = 0
+    counter += 1
+    print(counter)
+  
